45d7175a0103bfbd011b447f039400853c6c3f1d
sukantabhun <sukantabhun@gmail.com>
2024-10-15 08:13:17 +0530

    f

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import cors from "cors";
+import cors from "cors"; // Import cors
 
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
@@ -9,16 +9,28 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+// CORS Configuration
+app.use(cors({
+  origin: '*', // Allow this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // Enable cookies and auth headers
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
+
+// Middleware to parse JSON requests
 app.use(express.json({ limit: "50mb" }));
 
+// Routes
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
+// Root Route
 app.get("/", (req, res) => {
   res.send("Hello from DALL-E!");
 });
 
+// Start Server
 const startServer = async () => {
   try {
     await connectDB(process.env.MONGODB_URL);
@@ -26,7 +38,7 @@ const startServer = async () => {
       console.log("Server has started at http://localhost:8080");
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
